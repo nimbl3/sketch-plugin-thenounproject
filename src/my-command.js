3da905ed8ab192388e33161f4376a687fe6555c1
peepo3663 <peepo157@gmail.com>
2018-02-16 09:26:20 +0700

    Clean up all code that unused

--- src/my-command.js
+++ src/my-command.js
@@ -59,7 +59,6 @@ export function fetch(context) {
 
     function insertToSketch(imageData) {
         allLayers = doc.currentPage().layers()
-        // imagesCollection = loadSelectedImages(selection.count())
         for (var i = 0; i < selection.count(); i++) {
           layer = selection[i]
           if (layer.class() == MSShapeGroup) {
@@ -120,7 +119,6 @@ export function fetch(context) {
         COScript.currentCOScript().setShouldKeepAround(true);
 
         var contentView = panel.contentView();
-        var collectionView = NSCollectionView.alloc().initWithFrame(NSMakeRect(0, 0, frame.size.width, frame.size.height));
         for (var i = 0; i < allImages.length; i++) {
             var row = parseInt(i / 5)
             var column = i % 5
@@ -141,43 +139,10 @@ export function fetch(context) {
             contentView.addSubview(imageView);
         }
                 
-        //Flow layout
-        var flowLayout = NSCollectionViewFlowLayout.alloc().init()
-        flowLayout.setItemSize(NSMakeSize(80, 80))
-        flowLayout.setMinimumLineSpacing(5.0)
-        flowLayout.setMinimumInteritemSpacing(5.0)
-        
-        collectionView.setBackgroundColor(NSColor.whiteColor())
-        collectionView.setCollectionViewLayout(flowLayout);
-
-        // var itemNib = NSNib.alloc().initWithNibNamed_bundle("Item", null)
-        // collectionView.registerNib_forItemWithIdentifier(itemNib, "CollectionViewItem")
-        
-        //datasource & delegate
-        //var delegate = new MochaJSDelegate()
-        var dataSourceClass = new MochaJSDelegate({
-            "collectionView:itemForRepresentedObjectAtIndexPath:" : (function(collectionView, indexPath) {
-                var collectionViewItem = NSCollectionViewItem.alloc().init()
-                var imageUrlString = allImages[0]
-                collectionViewItem.imageView.setImage(NSImage.alloc().initWithContentsOfURL(NSURL.URLWithString(imageUrlString)))
-                return collectionViewItem
-            }),
-            "collectionView:numberOfItemsInSection:" : (function(collectionView, section) {
-                return 5
-            }) 
-        })
-        var dataSource = dataSourceClass.getClassInstance()
-
-        // collectionView.setDelegate(delegate)
-        collectionView.setDataSource(dataSource)
-        // collectionView.reloadData()
-
         contentView.setWantsLayer(true);
         var contentViewLayer = contentView.layer()
         contentViewLayer.setFrame( contentView.frame() );
         contentViewLayer.setMasksToBounds(true);
-
-        //contentView.addSubview(imageView);
         
         var closeButton = panel.standardWindowButton(NSWindowCloseButton)
         closeButton.setCOSJSTargetFunction(function(sender) {
@@ -203,82 +168,3 @@ export function fetch(context) {
     
     activate();
 }
-
-
-//-------------------------------------------------------------------------------------------------------------
-
-
-// GET GPLAY ICON BY NAME OF APP
-
-
-// function getGplayAppicon(context) {
-//   var doc = context.document;
-//   var selection = context.selection
-
-//     function alert(msg, title) {
-//         var app = [NSApplication sharedApplication];
-//         [app displayDialog:msg withTitle:title || "Error"];
-//     }
-
-//     function getGplayiconModal() {
-
-//         var userInput = [doc askForUserInput:"Enter any Android app's name (i.e. Facebook)):" initialValue:""];
-
-//         if (userInput !== null) {
-
-
-//             if (userInput !== null) {
-
-//                 fetchThatGplayicon(userInput);
-//             }
-//             else {
-//                  alert('error', 'Destination shape');
-//             }
-//         }
-//     }
-
-//     function fetchThatGplayicon(appName) {
-//         var apiURL      = [NSURL URLWithString:'https://api.import.io/store/data/8dda5db7-deee-44a7-90af-9a49b603b82b/_query?input/webpage/url=https%3A%2F%2Fplay.google.com%2Fstore%2Fsearch%3Fq%3D'+ appName +'%26c%3Dapps&_user=cdb830ea-85cb-4488-8ccd-cd9c6e7a9488&_apikey=cdb830ea85cb44888ccdcd9c6e7a94888e57b45cbdfd7aa6287207d1b2bd54e77f98b99b9d4b9fced1f5e4b1fb7784c7528712988270767e8f642719bcce6a21759beb8d77971dd2f378ac375acfc95c'];
-//         var request     = [NSURLRequest requestWithURL:apiURL];
-//         var response    = NSURLConnection.sendSynchronousRequest_returningResponse_error(request, null, null);
-
-//         var json = [NSJSONSerialization JSONObjectWithData:response options:null error:null];
-
-//         var imgUrlString = json.results[0].image_icon;
-//         var imgURL      = [NSURL URLWithString:imgUrlString];
-//         var imgRequest     = [NSURLRequest requestWithURL:imgURL];
-//         var img = NSURLConnection.sendSynchronousRequest_returningResponse_error(imgRequest, null, null);
-
-//         var GplayiconImage = [[NSImage alloc] initWithData:img];
-// 	    var GplayiconImageData = [[MSImageData alloc] initWithImage:GplayiconImage convertColorSpace:false];
-//         var allLayers = [[doc currentPage] layers];
-//         for (var i = 0; i < [selection count]; i++) {
-//             var layer = selection[i];
-//             if ([layer class] == MSShapeGroup) {
-//                 fill = layer.style().fills().firstObject();
-//                 coll = fill.documentData().images();
-//                 //[fill setPatternImage:GplayiconImage collection:coll];
-//                 fill.setFillType(4);         // Pattern fillType
-//                 fill.setPatternFillType(1);
-//                 fill.setIsEnabled(true);
-//                 [fill setImage:GplayiconImageData];
-//             }
-//         }
-
-//     }
-
-
-
-//     function activate() {
-//         log(selection[0].frame());
-//         if ([selection count] == 0) {
-//             alert('Please select a shape to fill the Google Play app icon into.', 'Destination shape');
-//         }
-//         else {
-//             getGplayiconModal();
-//         }
-//     }
-
-//     activate();
-
-// };
